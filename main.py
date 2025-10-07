5694ff8c9005f2d01e58747ffb4173094a122bac
unknown <orgalorg8732@gmail.com>
2025-10-07 10:51:00 +0800

    restarting merge

--- main.py
+++ main.py
@@ -2,17 +2,6 @@ num1=float(input("Enter first number:"))
 num2=float(input("Enter second number:"))
 choice=input("Enter choice (+,-,*,/)")
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-def miltiply (a, b):
-    return a * b
-=======
-def divide (a, b):
-    return a / b
->>>>>>> origin/Div
-
->>>>>>> origin/Mul
 if choice=='+':
     print(f"Result:{add(num1,num2)}")
 elif choice=='-':
